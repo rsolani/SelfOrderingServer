bb74b1e0be19fb22d43dc866ee2073c8b38815cc
rsolani <rsolani@gmail.com>
2015-06-12 03:37:20 -0300

    ...

---
+++ SelfOrdering.Api/App_Start/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using SelfOrdering.Api;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace SelfOrdering.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureWebApi(config);
+            app.UseCors(CorsOptions.AllowAll);
+            ConfigureOAuth(app);
+            
+        }
+
+        private static void ConfigureWebApi(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+
+        private static void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions oAuthServerOptions = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(20),
+                Provider = new AuthorizationServerProvider()
+            };
+
+            app.UseOAuthAuthorizationServer(oAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+           
+        }
+    }
+}
